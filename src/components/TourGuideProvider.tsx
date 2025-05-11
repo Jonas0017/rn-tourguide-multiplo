d9d52be86984a80ac0821c3d8aebc5b56e9826b6
jonas0017 <jonasavila@hotmail.com>
2025-05-11 20:52:24 -0300

    build: lib compilado com suporte a múltiplos tutoriais e compatível com React Native 0.74

--- src/components/TourGuideProvider.tsx
+++ src/components/TourGuideProvider.tsx
@@ -91,7 +91,9 @@ export const TourGuideProvider = ({
     if (mounted) {
       if (steps[tourKey]) {
         if (
-          (Array.isArray(steps[tourKey]) && steps[tourKey].length > 0) ||
+          //(Array.isArray(steps[tourKey]) && steps[tourKey].length > 0) ||
+          (Array.isArray(steps[tourKey]) && (steps[tourKey] as IStep[]).length > 0) ||
+
           Object.entries(steps[tourKey]).length > 0
         ) {
           setCanStart((obj) => {
